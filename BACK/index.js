fb9bbf83578453ad318e23ae306d6202dee1fd59
Кирилл Михайлец <kirillmihajlec@MacBook-Pro-Kirill.local>
2022-02-10 17:50:53 +0300

    hi

---
+++ BACK/index.js
@@ -0,0 +1,34 @@
+import { Express } from "express";
+import { Mongoose } from "mongoose";
+
+const app = express()
+const port = 3001
+
+mongoose.connect("mongodb+srv://School-It:Password12345@test-back-it.rv5t7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ,
+    {
+        useNewUrlParser: true,
+    })
+.then(() => console.log('MongoDb ok'))
+.catch(() => console.log('MongoDb error'))
+
+const Note = new mongoose.Schema({
+    noteText: {
+        type: String,
+        required: true,
+    }
+});
+
+const NoteDB = mongoose.model('Notes', Note);
+
+app.listen(port, () => {
+    console.log(`Server starting on ${port}`)
+})
+
+// app.use(function (req,
+//     res,
+//     next) {
+// res.header("Access-Control-Allow-Origin", "*");
+// res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+// res.header("Access-Control-Allow-Headers", "Content-Type");
+// next();
+// })
